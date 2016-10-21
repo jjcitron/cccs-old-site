fb07a4eea3dee6e46f185f89237cd9315457fa07
Jamie Brathwaite <jbrathwaite@aimediagroup.com>
2016-10-21 09:38:48 -0400

    test 2

--- local.php
+++ local.php
@@ -4,6 +4,7 @@ ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(-1);
 */
+
 require_once ('includes/Mobile_Detect.php');
 $detect = new Mobile_Detect;
 include('includes/pageSettings.php'); 	
