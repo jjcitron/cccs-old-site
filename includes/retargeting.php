7e02baded7d64bd59c4e873034e455c17f92fdd3
Jamie Brathwaite <jbrathwaite@aimediagroup.com>
2017-02-24 12:12:05 -0500

    added adroll pixel

--- includes/retargeting.php
+++ includes/retargeting.php
@@ -14,3 +14,25 @@ window._fbq = window._fbq || [];
 window._fbq.push(['track', 'PixelInitialized', {}]);
 </script>
 <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=1487662371499810&amp;ev=PixelInitialized" /></noscript>
+
+<script type="text/javascript">
+    adroll_adv_id = "Y2LE6DXV2ZE4FGNT6ZURGR";
+    adroll_pix_id = "S44RTS4P6VHNVNLWXPF6YL";
+    /* OPTIONAL: provide email to improve user identification */
+    /* adroll_email = "username@example.com"; */
+    (function () {
+        var _onload = function(){
+            if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}
+            if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}
+            var scr = document.createElement("script");
+            var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+            scr.setAttribute('async', 'true');
+            scr.type = "text/javascript";
+            scr.src = host + "/j/roundtrip.js";
+            ((document.getElementsByTagName('head') || [null])[0] ||
+                document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+        };
+        if (window.addEventListener) {window.addEventListener('load', _onload, false);}
+        else {window.attachEvent('onload', _onload)}
+    }());
+</script>
