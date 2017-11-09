8b0750aae61869caa0b560849755f89c09586a9a
joel <jcitron@aimediagroup.com>
2017-11-09 13:55:14 -0500

    move tags to GTM

--- home.php
+++ home.php
@@ -8,6 +8,13 @@ include('includes/pageContentSettings.php');
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5FB47D7');</script>
+<!-- End Google Tag Manager -->
 <title>Consolidated Credit Counseling – Debt Counseling, Debt Consolidation</title>
 <link type="image/x-icon" href="favicon.ico" rel="shortcut icon">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -52,20 +59,15 @@ else{echo'width=device-width, initial-scale=1.0';}
 </style>
 <![endif]-->
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-23415831-39', 'auto');
-  ga('require', 'displayfeatures');
-  ga('send', 'pageview');
 
-</script>
 </head>
 
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FB47D7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <?php include('includes/retargeting.php');?>
 <div class="container">
 
@@ -183,14 +185,6 @@ $('input, textarea').placeholder();
 	
 
 
-<script src="https://tracking.aimediagroup.com/aipx_secure.js" type="text/javascript"></script>
-<script type="text/javascript">
-	// for domain gus-lp.aiprx.consolidatedcredit.org
-	var cid = "";
-	_setAitrkCookie("WICOCdEC4LHTtrECWc");
-</script>	
-<script>(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"5189530"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");</script><noscript><img src="//bat.bing.com/action/0?ti=5189530&Ver=2" height="0" width="0" style="display:none; visibility: hidden;" /></noscript>
-  
 </body>
 
 
--- includes/thanks.php
+++ includes/thanks.php
@@ -46,61 +46,11 @@
 			document.write('<a class="button" target="_blank" href="https://www.consolidatedcredit.org/online-application/'+window.location.hash.substr(1)+'&partnerid=<!?php echo $partnerid;?>">			Continue Your Analysis</a>');
 			</script-->
        <!-- Google Code for Form Submission Complete Conversion Page -->
-<script type="text/javascript">
-/* <![CDATA[ */
-var google_conversion_id = 969826800;
-var google_conversion_language = "en";
-var google_conversion_format = "3";
-var google_conversion_color = "ffffff";
-var google_conversion_label = "G4pkCMiezggQ8MO5zgM";
-var google_remarketing_only = false;
-/* ]]> */
-</script>
-<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-</script>
-<noscript>
-<div style="display:inline;">
-<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/969826800/?label=G4pkCMiezggQ8MO5zgM&amp;guid=ON&amp;script=0"/>
-</div>
-</noscript>
-<script type="text/javascript"> if (!window.mstag) mstag = {loadTag : function(){},time : (new Date()).getTime()};</script> <script id="mstag_tops" type="text/javascript" src="//flex.msn.com/mstag/site/64e1e7b2-ad7d-487b-aaaa-1c878109d0f7/mstag.js"></script> <script type="text/javascript"> mstag.loadTag("analytics", {dedup:"1",domainId:"3084241",type:"1",actionid:"244367"})</script> <noscript> <iframe src="//flex.msn.com/mstag/tag/64e1e7b2-ad7d-487b-aaaa-1c878109d0f7/analytics.html?dedup=1&domainId=3084241&type=1&actionid=244367" frameborder="0" scrolling="no" width="1" height="1" style="visibility:hidden;display:none"> </iframe> </noscript>
 
 
-<script>(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"4019504"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");</script>
 
-<!-- Facebook Conversion Code for Form Sign Up -->
-<script>(function() {
-var _fbq = window._fbq || (window._fbq = []);
-if (!_fbq.loaded) {
-var fbds = document.createElement('script');
-fbds.async = true;
-fbds.src = '//connect.facebook.net/en_US/fbds.js';
-var s = document.getElementsByTagName('script')[0];
-s.parentNode.insertBefore(fbds, s);
-_fbq.loaded = true;
-}
-})();
-window._fbq = window._fbq || [];
-window._fbq.push(['track', '6023559873183', {'value':'0.01','currency':'USD'}]);
-</script>
-<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6023559873183&amp;cd[value]=0.01&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
 
-<!--Facebook Remarketing-->
-<script>(function() {
-  var _fbq = window._fbq || (window._fbq = []);
-  if (!_fbq.loaded) {
-    var fbds = document.createElement('script');
-    fbds.async = true;
-    fbds.src = '//connect.facebook.net/en_US/fbds.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(fbds, s);
-    _fbq.loaded = true;
-  }
-  _fbq.push(['addPixelId', '1487662371499810']);
-})();
-window._fbq = window._fbq || [];
-window._fbq.push(['track', 'PixelInitialized', {}]);
-</script>
-<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=1487662371499810&amp;ev=PixelInitialized" /></noscript>
+
+
 	
 </div>
